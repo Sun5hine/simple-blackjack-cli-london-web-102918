f1833b866b9666bd95175aac69e2979f20891730
sun5hine <af5kh@yahoo.com>
2018-10-20 18:24:25 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -42,7 +42,9 @@ invalid_command
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
+  prompt_user
+  get_user_input
 end
 
 #####################################################
